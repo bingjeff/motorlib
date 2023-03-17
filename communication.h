cf5908002df72b697102287fc992930e2b56011b
Jeff Bingham <bingjeff@gmail.com>
2023-03-17 09:35:22 -0700

    Add info methods to CommunicationBase.

--- communication.h
+++ communication.h
@@ -6,7 +6,10 @@
 class CommunicationBase {
  public:
     int receive_data(ReceiveData * const data) { return 0; }
+    bool receive_ready() {return false;}
+
     void send_data(const SendData &data) {}
+    bool send_acknowledged() {return false;}
 };
 
 #endif  // UNHUMAN_MOTORLIB_COMMUNICATION_H_
