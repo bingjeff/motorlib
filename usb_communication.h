600fb4ea05516fb8af93e1be032a4586ca74aef7
Jeff Bingham <bingjeff@gmail.com>
2023-03-18 16:33:57 -0700

    Make comms follow virtual/override from base.

--- usb_communication.h
+++ usb_communication.h
@@ -6,28 +6,28 @@
 class USBCommunication : public CommunicationBase {
  public:
   USBCommunication(USB1 &usb) : usb_(usb) {}
-  int receive_data(ReceiveData *const data) {
+  int receive_data(ReceiveData *const data) override {
     return usb_.receive_data(2, (uint8_t *const)data, sizeof(*data));
   }
-  bool receive_ready() { return usb_.new_rx_data(2); }
-  void send_data(const SendData &data) {
+  bool receive_ready() override { return usb_.new_rx_data(2); }
+  void send_data(const SendData &data) override {
     usb_.send_data(2, reinterpret_cast<const uint8_t *>(&data),
                    sizeof(SendData), false);
   }
-  bool send_acknowledged() { return usb_.tx_data_ack(2); }
+  bool send_acknowledged() override { return usb_.tx_data_ack(2); }
 
-  int receive_string(char *const string) {
+  int receive_string(char *const string) override {
     int count = usb_.receive_data(1, (uint8_t *const)string, 64);
     string[count] = 0;
     return count;
   }
-  bool send_string(const char *const string, uint16_t length) {
+  bool send_string(const char *const string, uint16_t length) override {
     usb_.send_data(1, (const uint8_t *const)string,
                    std::min((uint16_t)MAX_API_DATA_SIZE, length), true);
     return true;
   }
-  bool send_string_active() const { return usb_.tx_active(1); }
-  void cancel_send_string() { usb_.cancel_transfer(1); }
+  bool send_string_active() const override { return usb_.tx_active(1); }
+  void cancel_send_string() override { usb_.cancel_transfer(1); }
 
   uint32_t get_error_count() { return usb_.get_error_count(); }
 
